4eff9fa36f184f626d019e448d442e6be10502e2
daw16-15 <daw16-15@c16x11.iescierva.net>
2016-10-17 21:00:27 +0200

    commit 3

---
+++ Practica3/src/Gato.java
@@ -0,0 +1,25 @@
+/**
+ * Gato                                                       .java
+ * Programa ejemplo que muestra un gato
+ * Ramón Mayor López
+ */
+
+public class Gato 
+{
+	public static void main(String argumentos[])
+	{	
+	
+		System.out.println("                   |\\_/|     ");
+		System.out.println("                  / @ @ \\    ");
+		System.out.println("                 ( > º < )    ");
+		System.out.println("                  `»»x««´     ");
+		System.out.println("                  /  O  \\    ");
+		System.out.println("                 /   O   \\   ");
+		System.out.println("                /    O    \\  ");
+		System.out.println("               / ||  O  || \\ ");
+		System.out.println("              ( _||_ _ _||_ ) ");
+		System.out.println("                 **     **    ");
+		
+	}
+
+}

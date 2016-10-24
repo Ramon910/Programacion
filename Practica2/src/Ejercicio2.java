58612c5fd56df90b29e79c77bb3ce6ae2609af34
daw16-15 <daw16-15@c16x11.iescierva.net>
2016-10-24 18:44:26 +0200

    d

--- Practica2/src/Ejercicio2.java
+++ Practica2/src/Ejercicio2.java
@@ -1,23 +1,48 @@
 /**
- *Ejercicio2.java
- * programa simple que pida un nombre desde el teclado y lo guarde en una variable de tipo adecuado.
+ *Ejercicio3.java
+ * Escribe un programa simple que calcule el interés producido y 
+ * el capital total acumulado de un capital inicial invertido a un tipo de interés anual.
  * Ramón Mayor López
  */
-import java.util.Scanner;//importamos el paquete Scanner de la biblioteca
+import java.util.Scanner; //Importamos la clase Scanner 
+import java.lang.Math; //Importamos la clase Math de la biblioteca del API Java
+
 public class Ejercicio2 
 {
 	public static void main(String argumentos[])
-	{	//creamos una entrada por teclado y creamos una cadena de texto
+	{
+		//Creamos una entrada por teclado y creamos las variables necesarias
         Scanner teclado = new Scanner(System.in); 
-		
-        String nombre = "" ;
-        //pedimos el nombre y lo guardamos en la variable nombre y lo mostramos
-	System.out.println("Introduce tu nombre");
+		int n ; //numero de años
+        double r ;//interes
+        double cf; //capital final
+        int ci;//capital inicial
+        double i ; //interes 
+     // Pedimos las entradas por teclado   
+	System.out.println("Introduce el capital inical");
+	ci = teclado.nextInt();
 	
-	nombre = teclado.nextLine();
+	System.out.println("Introduce el interes");
+	r = teclado.nextDouble();	
 	
-	System.out.println("" +nombre );
-		
+	System.out.println("Introduce el número de años");
+	n = teclado.nextInt();	
+	//Mostramos los valores introducidos
+	System.out.println("El capital inicial es " +ci);  
+	System.out.println("El interes es de un "+r+"%");
+	System.out.println("El nñumero de años es de "+n);
+	
+	//calculamos el interes y lo mostramos
+	i = (1 + r) ; 
+       i = (double)Math.pow(i, n);   // calcula num2
+       
+       System.out.println("El interes en "+n+" años es de " +i);
+       //calculamos el capital final y lo mostramos
+       cf = ci * i ;
+       
+       System.out.println("El capital final es de "+cf);
+       
+	
 	}
 
 }
--- Practica2/src/Ejercicio3.java
+++ Practica2/src/Ejercicio3.java
@@ -1,48 +1,26 @@
 /**
- *Ejercicio3.java
- * Escribe un programa simple que calcule el interés producido y 
- * el capital total acumulado de un capital inicial invertido a un tipo de interés anual.
+ *Ejercicio4.java
+ * programa simple que lea exactamente 8 números enteros y luego escriba la suma de todos ellos.
  * Ramón Mayor López
  */
-import java.util.Scanner; //Importamos la clase Scanner 
-import java.lang.Math; //Importamos la clase Math de la biblioteca del API Java
-
+import java.util.Scanner;//importamos el paquete Scanner de la biblioteca
 public class Ejercicio3 
 {
 	public static void main(String argumentos[])
-	{
-		//Creamos una entrada por teclado y creamos las variables necesarias
-        Scanner teclado = new Scanner(System.in); 
-		int n ; //numero de años
-        double r ;//interes
-        double cf; //capital final
-        int ci;//capital inicial
-        double i ; //interes 
-     // Pedimos las entradas por teclado   
-	System.out.println("Introduce el capital inical");
-	ci = teclado.nextInt();
-	
-	System.out.println("Introduce el interes");
-	r = teclado.nextDouble();	
-	
-	System.out.println("Introduce el número de años");
-	n = teclado.nextInt();	
-	//Mostramos los valores introducidos
-	System.out.println("El capital inicial es " +ci);  
-	System.out.println("El interes es de un "+r+"%");
-	System.out.println("El nñumero de años es de "+n);
-	
-	//calculamos el interes y lo mostramos
-	i = (1 + r) ; 
-       i = (double)Math.pow(i, n);   // calcula num2
-       
-       System.out.println("El interes en "+n+" años es de " +i);
-       //calculamos el capital final y lo mostramos
-       cf = ci * i ;
-       
-       System.out.println("El capital final es de "+cf);
-       
-	
+	{	// creamos una entrada por teclado y creamos las variables
+		int n=0;
+		int i ;
+		Scanner teclado=new Scanner(System.in);
+		
+		//con un bucle vamos pidiendo los valores y sumandolos
+		for ( i = 0; i < 8; i++) 
+		{
+			System.out.println("Introduce un número: ");
+			n=n + teclado.nextInt();
+		}
+	// mostramos la suma 
+        System.out.println("El número total es "+n);
+		
 	}
-
+	
 }
